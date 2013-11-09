429ae7e240e57df3a4a7bb0809bb786f7984e9f3
Pauli Perälä <perala.pauli@gmail.com>
2013-11-09 23:30:21 +0200

    added firefox to test runners

--- karma.conf.js
+++ karma.conf.js
@@ -45,7 +45,10 @@ module.exports = function(config) {
     // - Safari (only Mac)
     // - PhantomJS
     // - IE (only Windows)
-    browsers: ['Chrome'],
+    browsers: [
+      'Chrome',
+      'Firefox'
+    ],
 
 
     // Continuous Integration mode
