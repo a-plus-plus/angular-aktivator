8d18699f134eb25e7f19ee370f20a252280a0c5a
Pauli Perälä <perala.pauli@gmail.com>
2013-11-10 00:54:49 +0200

    removed unneeded loadNpmTask
    handled by load-grunt-tasks at line 11

--- Gruntfile.js
+++ Gruntfile.js
@@ -10,7 +10,6 @@
 module.exports = function (grunt) {
   require('load-grunt-tasks')(grunt);
   require('time-grunt')(grunt);
-  grunt.loadNpmTasks('grunt-notify');
 
   grunt.initConfig({
     yeoman: {
