f81d4dcd80a7f87c363d6b79b29cfb8bd48452b6
Jenna Lindh <jenna.lindh@luukku.com>
2013-11-27 18:52:43 +0200

    test conf

--- protractor.conf.js
+++ protractor.conf.js
@@ -43,7 +43,7 @@ exports.config = {
   // Spec patterns are relative to the location of this config.
   specs: [
     //'spec/*_spec.js',
-    '.tmp/protractor/answering_survey.js'
+    '.tmp/protractor/*.js'
   ],
 
   // ----- Capabilities to be passed to the webdriver instance ----
